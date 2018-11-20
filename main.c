b8867ff4912fdddc120a204a1a12418a38729592
ACJMR <chosking@stuy.edu>
2018-11-19 23:38:32 -0500

    added forking

--- main.c
+++ main.c
@@ -2,22 +2,36 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 char ** parse_args( char * line ){
     int end = strlen(line) - 1;
-    if(line[end] == '\n')
+    if(line[end] == '\n'){
         line[end] = '\0';
-
+    }
+    
 	char ** arglist = calloc(sizeof(char*), 25); // 25 args max
 	for(int i = 0; (arglist[i] = strsep(&line, " ")); i++);
 	return arglist;
 }
 
+void executeBuf(char * cmd_buffer){
+    int f = fork();
+    if (f){ //PARENT 
+        int status, childpid;
+        childpid = wait(&status);
+    }
+    else{ //CHILD
+        fgets(cmd_buffer, 100, stdin);
+        char ** args = parse_args(cmd_buffer);
+        execvp(args[0], args);
+    }
+    return;
+}
+
 int main(){
     char * cmd_buffer = calloc(1, 100);
     printf("casch$ ");
-    fgets(cmd_buffer, 100, stdin);
-    char ** args = parse_args(cmd_buffer);
-    execvp(args[0], args);
+    executeBuf(cmd_buffer);
     return 0;
 }
