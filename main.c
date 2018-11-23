486f2fa156f31209da26d89fbecdae88a4c6da1d
ACJMR <chosking@stuy.edu>
2018-11-23 10:12:30 -0500

    added

--- main.c
+++ main.c
@@ -10,8 +10,14 @@ int custom_commands(char ** args){
         exit(0);
         return 1;
     } else if(!strcmp(args[0], "cd")){
-        chdir(args[1]);
+        if (chdir(args[1])){
+          printf("-casch: %s: --directry not found--\n",args[1]);
+        }
         return 1;
+    }
+    else if(!strcmp(args[0], "h")){
+      printf("ha ha\n");
+      return 1;
     }
     return 0;
 }
@@ -35,13 +41,18 @@ void fork_exec(char ** args){
 }
 
 int main(){
+    char ** history = calloc(50, 100); //stores up to 100 commands of 50 chars max
+    int hist_size = 0;
     char * cmd_buffer = calloc(1, 100);
     while(1){
         char * cwd_buffer = calloc(1, 100);
         getcwd(cwd_buffer, 100);
         printf("%s casch$ ", cwd_buffer);
-        
+
         fgets(cmd_buffer, 100, stdin);
+        history[hist_size] = cmd_buffer;
+        hist_size++;
+
         char ** cmds = parse_line(cmd_buffer);
         for(int i = 0; cmds[i]; i++){   // Iterate through commands
             char ** args = parse_args(cmds[i]);
@@ -49,6 +60,7 @@ int main(){
         }
         free(cwd_buffer);
     }
+    free(history);
     free(cmd_buffer);
     return 0;
 }
